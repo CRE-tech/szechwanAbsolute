d53ad35ce6091eec2bfef83d8f975d867e17ea4e
bearclimber <vincentchsieh@gmail.com>
2017-07-09 12:37:27 -0400

    logo and reviews done, need to work on about page description

--- sa/public/javascripts/loadmore.js
+++ sa/public/javascripts/loadmore.js
@@ -40,3 +40,10 @@ function Load2() {
     }
 }
 
+$('.maps').click(function () {
+    $('.maps iframe').css("pointer-events", "auto");
+});
+
+$( ".maps" ).mouseleave(function() {
+  $('.maps iframe').css("pointer-events", "none"); 
+});
